271bcf6bdf3290918f8de129329f6928e60b4f33
Muhammet <49239301+SaySaviour@users.noreply.github.com>
2022-09-26 14:01:59 +0300

    Admin-Layout panel created

---
+++ Core_MVC_Proje/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_MVC_Proje.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+    }
+}
