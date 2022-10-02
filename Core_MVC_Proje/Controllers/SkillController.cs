2edc35c8a60a8cefd73796ab892d2fc46cf83f48
Muhammet <49239301+SaySaviour@users.noreply.github.com>
2022-10-02 15:35:09 +0300

    NavigationBar updated

--- Core_MVC_Proje/Controllers/SkillController.cs
+++ Core_MVC_Proje/Controllers/SkillController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrate;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_MVC_Proje.Controllers
@@ -9,8 +10,25 @@ namespace Core_MVC_Proje.Controllers
         SkillManager skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            ViewBag.v1 = "Yetenek Listesi";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Listesi";
             var values = skillManager.GetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            ViewBag.v1 = "Yetenek Ekleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Ekleme";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index");
         }
     }
 }
