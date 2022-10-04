2da7cbc6b1aa65c2343af86f392878c45f539b96
Muhammet <49239301+SaySaviour@users.noreply.github.com>
2022-10-04 15:18:20 +0300

    Experience Crud completed

---
+++ Core_MVC_Proje/Controllers/ExperienceController.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrate;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Core_MVC_Proje.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Listeleme";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim Listeleme";
+            var values= experienceManager.GetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddExperience()
+        {
+            ViewBag.v1 = "Ekleme";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim Ekleme";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience)
+        {
+            experienceManager.TAdd(experience);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var values = experienceManager.TGetByID(id);
+            experienceManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditExperience(int id)
+        {
+            ViewBag.v1 = "Güncelleme";
+            ViewBag.v2 = "Deneyimler";
+            ViewBag.v3 = "Deneyim Güncelleme";
+            var values = experienceManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditExperience(Experience experience)
+        {
+            experienceManager.TUpdate(experience);
+            return RedirectToAction("Index");
+        }
+    }
+}
