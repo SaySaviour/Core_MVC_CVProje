3ec6489baf5da8b68f7e6983e16cd213e8c1527a
Muhammet <49239301+SaySaviour@users.noreply.github.com>
2022-10-05 17:43:58 +0300

    Portfolio projects added

---
+++ Core_MVC_Proje/Controllers/PortfolioController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_MVC_Proje.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Listesi";
+            var values = portfolioManager.GetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            ViewBag.v1 = "Proje Ekle";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Ekle";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio portfolio)
+        {
+            portfolioManager.TAdd(portfolio);
+            return RedirectToAction("Index");
+        }
+    }
+}
