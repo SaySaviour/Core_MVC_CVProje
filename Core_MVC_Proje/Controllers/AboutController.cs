545d5cfbbc0e76dc5e736e620df83397e249bad4
Muhammet <49239301+SaySaviour@users.noreply.github.com>
2022-11-02 15:16:00 +0300

    Service-Feature-About , for admin pages created

---
+++ Core_MVC_Proje/Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_MVC_Proje.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager=new AboutManager(new EfAboutDal());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Güncelleme";
+            ViewBag.v2 = "Hakkımda";
+            ViewBag.v3 = "Hakkımda Sayfası Güncelleme";
+            var values = aboutManager.TGetByID(1);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult Index(About about)
+        {
+            aboutManager.TUpdate(about);
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
